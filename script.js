8484fec733a89a2e28c2106693af261260ced8b1
Bielorusse <thibautvoirand@gmail.com>
2019-01-26 15:39:08 +0100

    first version

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-document.getElementById("quote_div").innerHTML = "This is a test quote."
+
+quotes = [
+    "Test quote number 1",
+    "Test quote number 2",
+];
+
+function pick_quote(){
+    `
+    Pick a random quote from the database
+    `
+    id = Math.floor(Math.random() * quotes.length);
+    document.getElementById("quote_div").innerHTML = quotes[id];
+}
