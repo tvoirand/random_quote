319fd68cf1131926e573714218d5e27d9d4fbb35
Bielorusse <thibautvoirand@gmail.com>
2019-09-15 13:04:10 +0200

    changed database file type to json

--- readme.txt
+++ readme.txt
@@ -1,10 +1,9 @@
 Random_quote
 
-Version 0.0.2 of the 20190127.
+Version 0.0.3 of the 20190915.
 
 The purpose of this project is to display a random quote taken from a list on my personal website.
 
 Added in this version:
-    -added quotes database xml file
-    -display_random_quote function added in script
-    -read_database function updated
+    -changed database file type to json
+    -updated script to handle json file
--- script.js
+++ script.js
@@ -1,64 +1,60 @@
 
 function display_random_quote(){
     `
-    Open quotes database xml file, read it, and display a random quote.
+    Open quotes database json file, read it, and display a random quote.
     `
 
+    // load the database file
     var xhttp_request = new XMLHttpRequest();
-    xhttp_request.open("GET", "quotes_database.xml", true);
+    xhttp_request.open("GET", "database.json", true);
     xhttp_request.send();
 
     xhttp_request.onreadystatechange = function() {
 
         if (this.readyState == 4 && this.status == 200) {
 
-            quotes_array = read_database(this);
+            // store the json database in an array of javascript objects
+            quotes_array = JSON.parse(this.responseText).quotes;
 
+            // pick a random quote
             id = Math.floor(Math.random() * quotes_array.length);
-            quote_display_str = "";
+            quote = quotes_array[id]
 
-            for (col = 1; col < quotes_array[id].length; col++){
-                if (quotes_array[id][col] !== null && quotes_array[id][col] !== ""){
-                    if (col == 4){
-                        quote_display_str = quote_display_str.concat("page ")
-                    }
-                    if (col == 5) {
-                        quote_display_str = quote_display_str.concat("<br>")
-                    }
-                    quote_display_str = quote_display_str.concat(quotes_array[id][col])
-                    quote_display_str = quote_display_str.concat("<br>")
-                }
+            // initiate the string to display
+            display_string = "";
+
+            // add author
+            if (quote.author !== null && quote.author !== ""){
+                display_string = display_string.concat(quote.author)
+                display_string = display_string.concat("<br>")
             }
-            document.getElementById("quote_div").innerHTML = quote_display_str;
+
+            // add book
+            if (quote.book !== null && quote.book !== ""){
+                display_string = display_string.concat(quote.book)
+                display_string = display_string.concat("<br>")
+            }
+
+            // add edition and page only if both are available
+            if (quote.edition !== null && quote.edition !== "" && quote.page !== null && quote.page !== ""){
+                display_string = display_string.concat("Edition ")
+                display_string = display_string.concat(quote.edition)
+                display_string = display_string.concat("<br>")
+                display_string = display_string.concat("Page ")
+                display_string = display_string.concat(quote.page)
+                display_string = display_string.concat("<br>")
+            }
+
+            // add text
+            if (quote.text !== null && quote.text !== ""){
+                display_string = display_string.concat("<br>")
+                display_string = display_string.concat(quote.text)
+                display_string = display_string.concat("<br>")
+            }
+
+            // add display string to html
+            document.getElementById("quote_div").innerHTML = display_string;
         }
 
     };
-}
-
-function read_database(xml) {
-    `
-    Read quotes database xml file.
-    Output:
-        -quotes_array   [[str, ...], ...]
-            -contains for each quote: ID, AUTHOR, BOOK, EDITION, PAGE, TEXT
-    `
-
-    var xml_doc = xml.responseXML;
-    var quotes_array = [];
-    var quotes_xml = xml_doc.getElementsByTagName("QUOTE");
-
-    for (i = 0; i < quotes_xml.length; i++) {
-        quotes_array.push(
-            [
-                quotes_xml[i].getElementsByTagName("ID")[0].childNodes[0].nodeValue,
-                quotes_xml[i].getElementsByTagName("AUTHOR")[0].childNodes[0].nodeValue,
-                quotes_xml[i].getElementsByTagName("BOOK")[0].childNodes[0].nodeValue,
-                quotes_xml[i].getElementsByTagName("EDITION")[0].childNodes[0].nodeValue,
-                quotes_xml[i].getElementsByTagName("PAGE")[0].childNodes[0].nodeValue,
-                quotes_xml[i].getElementsByTagName("TEXT")[0].childNodes[0].nodeValue,
-            ]
-        );
-    }
-
-    return quotes_array;
 }
