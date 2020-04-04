7ea5b96593abf667c2d6412dbaa3271404590cf3
Bielorusse <thibautvoirand@gmail.com>
2020-04-04 20:33:13 +0200

    changed main structure and added live quote

--- random_quote.js
+++ random_quote.js
@@ -2,25 +2,92 @@
 Script for random_quote.
 */
 
-import { draw_words } from "./modules/vjing.js";
 import { xhr_success, xhr_error, load_file } from "./modules/utils.js";
 
 // load quotes database and fill quote div when page is loaded
 document.getElementById("index_body").onload = load_file(
     "database.json",
-    fill_quote_div
+    random_quote
 );
 
-function fill_quote_div() {
+function random_quote() {
     /*
-    Callback function for a quotes database reading XHR request to fill quote_div with a quote.
+    Main function for random quote.
+    Callback function which gets executed by a XHR request reading the database.json file.
     */
 
     // store the json database in an array of javascript objects
     let quotes_array = JSON.parse(this.responseText).quotes;
 
-    // pick a random quote
+    // get first random quote
     let quote = pick_random_quote(quotes_array);
+
+    // fill quote div on html page
+    fill_quote_div(quote);
+
+    //---------------------------------------------------------
+    // creating processing (p5js) sketch for the live quote part
+
+    // global variables
+    const canvas_width = 400;
+    const canvas_height = 300;
+    let word_index = 0; // initiate word index
+    let quote_list = quote.text.split(" "); // split the quote in a list of words
+    const s = sketch => {
+        /*
+        Function which takes a "sketch" object as argument and attaches properties such as setup and
+        draw functions which are needed for a p5js sketch.
+        See: https://github.com/processing/p5.js/wiki/Global-and-instance-mode
+        */
+
+        sketch.setup = function() {
+            /*
+            Processing setup function.
+            Called one time at start.
+            */
+            let canvas = sketch.createCanvas(canvas_width, canvas_height);
+            canvas.parent(canvas_div);
+            sketch.stroke(255);
+            sketch.frameRate(4);
+        };
+
+        sketch.draw = function() {
+            /*
+            Processing main loop function.
+            */
+
+            // initialisation
+            sketch.background(0);
+
+            // find new quote if end of current quote reached
+            if (quote_list.length == word_index) {
+                // pick the new quote and avoid picking the same quote
+                let new_quote = quote;
+                while (new_quote == quote) {
+                    new_quote = pick_random_quote(quotes_array);
+                }
+                quote = new_quote;
+
+                quote_list = quote.text.split(" "); // split the quote in a list of words
+                word_index = 0; // re-initiate the word count
+            }
+
+            // diplay the current word
+            sketch.text(quote_list[word_index], 10, 30);
+
+            // increase word count
+            word_index += 1;
+        };
+    };
+    let myp5 = new p5(s);
+}
+
+function fill_quote_div(quote) {
+    /*
+    Prepare display string and fill the quote div in the HTML page.
+    Input:
+        -quote  {author: str, book: str, edition: str, page: str, text: str}
+    */
 
     // initiate the string to display
     let output_string = "";
@@ -75,57 +142,3 @@ function pick_random_quote(quotes_array) {
 
     return quotes_array[id];
 }
-
-function draw_quote(sketch) {
-    /*
-    Callback function for a quotes database reading XHR request to draw a quote on a p5js sketch.
-    Input:
-        -sketch     p5js sketch
-    */
-
-    // store the json database in an array of javascript objects
-    let quotes_array = JSON.parse(this.responseText).quotes;
-
-    // pick a random quote
-    let quote = pick_random_quote(quotes_array);
-
-    // draw the quote
-    sketch.text(quote.text, 10, 30);
-}
-
-// creating processing sketch for the VJing part
-// global variables
-const canvas_width = 400;
-const canvas_height = 300;
-const s = sketch => {
-    /*
-    Function which takes a "sketch" object as argument and attaches properties such as setup and
-    draw functions which are needed for a p5js sketch.
-    See: https://github.com/processing/p5.js/wiki/Global-and-instance-mode
-    */
-
-    sketch.setup = function() {
-        /*
-        Processing setup function.
-        Called one time at start.
-        */
-        let canvas = sketch.createCanvas(canvas_width, canvas_height);
-        canvas.parent(canvas_div);
-        sketch.stroke(255);
-        sketch.frameRate(1);
-    };
-
-    sketch.draw = function() {
-        /*
-        Processing main loop function.
-        */
-
-        // initialisation
-        sketch.background(0);
-        // translate(width / 2, height / 2);
-        // background(0);
-
-        load_file("database.json", draw_quote, sketch);
-    };
-};
-let myp5 = new p5(s);
